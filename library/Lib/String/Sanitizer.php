a16eac62b3e4e6eb1d679eb2c1f8bbd5eab22cd2
Ryan McCullagh <rmccullagh@niu.edu>
2014-02-04 23:11:46 -0600

    added a method to escape html characters

--- library/Lib/String/Sanitizer.php
+++ library/Lib/String/Sanitizer.php
@@ -2,7 +2,9 @@
 namespace Lib\String;
 
 class Sanitizer {
-    public static function clean($string = NULL) {
-        return $string;
+    public static function xssClean($string) {
+        if(is_string($string)) {
+            return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+        }
     }
 }
